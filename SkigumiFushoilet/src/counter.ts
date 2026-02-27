4d4c58969e5deca6041bc9603f7347f627b46eda
Isadora Zhang <izzyorsomething@gmail.com>
2026-02-26 23:46:25 -0500

    Wrote a buncha shit for brainstorming in Ideas.txt and uploaded calc photo for some fuckin reason

--- SkigumiFushoilet/src/counter.ts
+++ SkigumiFushoilet/src/counter.ts
@@ -1,9 +1,9 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+export function counter(element: HTMLButtonElement) {
+    let counter = 0;
+    const setCounter = (count: number) => {
+        counter = count
+        element.innerHTML = `You've skigumized it ${counter} times.`
+    }
+    element.addEventListener('click', () => setCounter(counter + 1))
+    setCounter(0)
 }
