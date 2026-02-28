c1a2b87d3cb1d10ecd2d814eeed1f4f383842ae7
Isadora Zhang <izzyorsomething@gmail.com>
2026-02-28 00:15:16 -0500

    Messing around with CSS creating rectangle classes (its going bad help)

--- SkigumiFushoilet/src/main.ts
+++ SkigumiFushoilet/src/main.ts
@@ -1,15 +1,18 @@
 import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <html>
-<div>
-    <head>
-   Skigumi Fushoilet Website
-    </head>
-    <body>
-    <h> Heading </h>
-    <p> Yeah so I have no idea why I chose to make this </p>
-    </body>
-  </div>
-</html>
+    <div class="window_background">
+      <div class="window_individual">
+        <p> div </p>
+      </div>
+      <head>
+       <h1> Skigumi Fushoilet Website </h1>
+      </head>
+      <body>
+        <p> help i have no idea how to write html or css or js im so cooked</p>
+      </body>
+    </div>
+
+  </html>
 `
 
